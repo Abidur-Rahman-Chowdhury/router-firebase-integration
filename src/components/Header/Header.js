bee67a6d5a22b9187cd39a2251ee55c14c57f1b2
Abidur Rahman Chowdhury <abid43iiuc@gmail.com>
2022-04-09 15:40:01 +0600

    added private router

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,13 @@
+import { getAuth, signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
-import useFirebase from '../../hooks/useFirebase';
+import app from '../../firebase.init';
+
 import './Header.css'
+const auth = getAuth(app);
 const Header = () => {
-    const { user,handelSignOut } = useFirebase();
+    const [user] = useAuthState(auth)
     return (
         <div className='header'>
             <nav>
@@ -13,7 +17,7 @@ const Header = () => {
                 <Link to='/register'>Register</Link>
                 <span>{ user?.displayName && user.displayName}</span>
                 {user?.uid ? 
-                    <button onClick={handelSignOut}> Sign out</button>
+                    <button onClick={()=> signOut(auth)}> Sign out</button>
                     :
                     <Link to='/login'>Login</Link>
                 }
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,23 @@
+import { getAuth } from 'firebase/auth';
 import React from 'react';
-import useFirebase from '../../hooks/useFirebase';
+import {useSignInWithGoogle} from 'react-firebase-hooks/auth'
+import { useLocation, useNavigate } from 'react-router-dom';
+import app from '../../firebase.init';
 import './Login.css'
+const auth = getAuth(app);
 const Login = () => {
-    const { user, signInWithGoogle } = useFirebase();
+    let location = useLocation();
+    const navigate = useNavigate();
+    let from = location.state?.from?.pathname || "/";
+    const [signInWithGoogle, user] = useSignInWithGoogle(auth)
+    if (user) {
+        navigate(from, { replace: true });
+    }
     return (
         <div>
             <h3>Please login</h3> 
             <div style={{ margin: "20px" }}>
-            <button onClick={signInWithGoogle} >Google Sign In</button> 
+            <button onClick={() => signInWithGoogle()} >Google Sign In</button> 
             </div>
 
             <form >
